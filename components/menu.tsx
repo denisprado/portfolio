ed9d51675f91658c91c007e7735399b2220952d1
denisforigo <denis.forigo@mentora.gg>
2023-06-20 23:29:49 -0300

    fiz menu itens bug

--- components/menu.tsx
+++ components/menu.tsx
@@ -29,9 +29,26 @@ export function MenuItems({ items }: MenuItemsProps) {
 			{items.map(({ label, href }, i) => {
 
 				return i > 0 && (
-					<motion.div
-						className={classNames("relative px-4 py-1 m-0 font-sans cursor-pointer rounded-full mr-3", { "bg-neutral-dark-3 text-neutral-light-2": themeColor === 'dark', "bg-white text-neutral-dark-2": themeColor === 'light' })}
+					<Link href={href}
+						className="text-xs uppercase font-sans relative"
+						onPointerEnter={() => {
+							setStore({
+								activeColor: color,
+								selected: i,
+								before: store.before
+							})
+						}}
+						onPointerOut={() => {
+							setStore({
+								activeColor: color,
+								selected: store.before,
+								before: store.before
+							})
+						}}
+					><motion.div
+						className={classNames("relative px-3 py-2 m-0 font-sans cursor-pointer rounded-full mr-3", { "bg-neutral-dark-3 text-neutral-light-2": themeColor === 'dark', "bg-white text-neutral-dark-2": themeColor === 'light' })}
 						key={i}
+						style={{ zIndex: 999 }}
 						initial={{
 							color:
 								i === store.selected ? "hsl(var(--neutral-light-1))" : themeColor === 'light' ? "hsl(var(--neutral-dark-1))" : "hsl(var(--neutral-light-2))",
@@ -62,34 +79,13 @@ export function MenuItems({ items }: MenuItemsProps) {
 							})
 						}}
 					>
-						<Link href={href}
-							className="text-xs uppercase font-sans"
-							style={{ position: "relative", zIndex: 1 }}
-							onPointerEnter={() => {
+							<span onTap={() => {
 								setStore({
 									activeColor: color,
 									selected: i,
-									before: store.before
+									before: i,
 								})
 							}}
-							onPointerOut={() => {
-								setStore({
-									activeColor: color,
-									selected: store.before,
-									before: store.before
-								})
-							}}
-						>
-							{label}
-						</Link>
-						{i === store.selected && (
-							<motion.div
-								className={"w-full h-full absolute rounded-full top-0 left-0"}
-								layoutId="selected"
-								initial={{
-									backgroundColor: store.activeColor,
-								}}
-								animate={{ backgroundColor: color }}
 								onPointerEnter={() => {
 									setStore({
 										activeColor: color,
@@ -104,9 +100,37 @@ export function MenuItems({ items }: MenuItemsProps) {
 										before: store.before
 									})
 								}}
-							/>
-						)}
-					</motion.div>
+								className="z-10 relative">
+
+								{label}
+							</span>
+
+							{i === store.selected && (
+								<motion.div
+									className={"w-full h-full absolute rounded-full top-0 left-0"}
+									layoutId="selected"
+									initial={{
+										backgroundColor: store.activeColor,
+									}}
+									animate={{ backgroundColor: color }}
+									onPointerEnter={() => {
+										setStore({
+											activeColor: color,
+											selected: i,
+											before: store.before
+										})
+									}}
+									onPointerOut={() => {
+										setStore({
+											activeColor: color,
+											selected: store.before,
+											before: store.before
+										})
+									}}
+								/>
+							)}
+						</motion.div>
+					</Link>
 				)
 			})}
 		</div>
