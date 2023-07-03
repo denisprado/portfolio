c09b9222fd852754ca10e1bde6e181989041f937
Denis Forigo <denisforigo@gmail.com>
2023-07-02 23:33:49 -0300

    toggle theme added

--- components/header.tsx
+++ components/header.tsx
@@ -19,17 +19,20 @@ export const Header = () => {
 		{ href: "/trabalho", label: "trabalho" },
 	]
 
+	const page = usePathname()
+
 	const { color, setColor } = useThemeContext();
 
 	return (
 		<header className="relative z-10 w-full">
-			<Container className="flex flex-row items-center">
+			<Container className={classNames("flex flex-row items-center w-full", { 'absolute': page === '/' })}>
 
 				<Link href="/" className={classNames("h-16 pl-3 pr-5 pt-4 pb-3", { "bg-neutral-dark-3": color === 'dark' })}>
 					<motion.img src={color === 'light' ? "./images/logo.svg" : "./images/logo-servicos.svg"} width={200} height={40} />
 				</Link>
 
-				<nav className={classNames("w-full h-16 px-4 py-3 flex flex-col justify-center", { "bg-neutral-dark-1": color === 'dark' }, { "bg-neutral-light-1": color === 'light' })}>
+				<nav className={classNames("w-full h-16 px-4 py-3 flex flex-col justify-center bg-neutral-light-1 dark:bg-neutral-dark-1",
+					{ 'bg-transparent': page === '/' })}>
 					<ul className="[&_li]:ml-4 mr-6 flex justify-end items-center">
 						<MenuItems items={items} />
 						<li><ModeToggle /></li>
