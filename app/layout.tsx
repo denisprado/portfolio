a4266a2f341d2b39be0d8bb764deb7f4b6d5c36e
Denis Forigo <denisforigo@gmail.com>
2023-07-25 11:27:45 -0300

    change import font config

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,19 +16,20 @@ import React from "react"
 import { ThemeContextProvider } from "./context/theme"
 import './globals.css'
 
-const instrumentSans = Instrument_Sans({ subsets: ['latin'], display: 'swap', variable: '--font-instrument-sans' })
+const instrumentSans = Instrument_Sans({ subsets: ['latin'], display: 'swap', variable: '--font-instrument-sans',   adjustFontFallback: false} )
 const instrumentSerif = Instrument_Serif({
 	subsets: ['latin'],
 	display: 'swap',
 	variable: '--font-instrument-serif',
-	weight: "400"
+	weight: "400",
+	adjustFontFallback: false
 });
 
 const instrumentSerifItalic = Instrument_Serif({
 	subsets: ['latin'],
 	display: 'swap',
 	variable: '--font-instrument-italic',
-	weight: "400", style: 'italic'
+	weight: "400", style: 'italic',adjustFontFallback: false
 });
 
 export default function RootLayout({
