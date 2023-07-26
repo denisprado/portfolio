e0923d0b4d55c31bcfae3bebc9b1ed5f7b11d242
denisforigo <denis.forigo@mentora.gg>
2023-07-26 11:32:19 -0300

    thumbnail better

--- components/rowCard.tsx
+++ components/rowCard.tsx
@@ -37,16 +37,16 @@ const RowCard = ({ cards }: CardsProps) => {
 			cards && cards?.map(card => (
 				<FinalRow url={card?.url!} id={card?.id!} key={card?.id!}>
 					<div key={card.id} className="flex flex-col md:items-center md:flex-row justify-between dark:bg-neutral-dark-1 dark:text-neutral-light-2 dark:border-neutral-light-3 border-t bg-white border-neutral-dark-1 last:border-b p-12">
-						<div className='flex flex-row items-center justify-center gap-4'>
+						<div className='flex flex-row items-center justify-start w-full gap-4'>
 							{card.image && <div className='aspect-square w-24 h-24'>
 								<Image src={card.image} width={96 * 2} height={96 * 2} alt={card.title!} className={'grayscale'} style={{ objectFit: 'cover' }}></Image>
 							</div>
 							}
-							{card.thumbnail && <div className='aspect-square w-48 h-48'>
-								<Image src={getUrlFromTable(card.thumbnail)} width={96} height={96} alt={card.title ? card.title : ''} className={'grayscale'} style={{ objectFit: 'cover' }} />
+							{card.thumbnail && <div className='aspect-square w-96 h-48 max-h-full '>
+								<Image src={getUrlFromTable(card.thumbnail)} width={96 * 4} height={96 * 4} alt={card.title ? card.title : ''} style={{ objectFit: 'cover', objectPosition: 'center', maxHeight: '100%' }} />
 							</div>
 							}
-							{card.title && <h3 className={`${card.color} text-8xl`}>
+							{card.title && <h3 className={`${card.color} text-8xl `}>
 								<span className="font-sans">{card.title.substring(0, card.title.indexOf(" "))}</span>
 								<span className="font-serif">{card.title.substring(card.title.indexOf(" "))}</span>
 							</h3>}
