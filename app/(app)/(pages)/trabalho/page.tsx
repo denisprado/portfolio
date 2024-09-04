7693514277acec79e034b8c893f0d7f144ba2a4f
Denis Forigo <denisforigo@gmail.com>
2024-09-03 22:11:24 -0300

    better work single

--- app/(app)/(pages)/trabalho/page.tsx
+++ app/(app)/(pages)/trabalho/page.tsx
@@ -1,6 +1,7 @@
 import WorkCard, { RowCardProps } from "@/components/workCard"
 import configPromise from '@payload-config'
 import { getPayloadHMR } from "@payloadcms/next/utilities"
+import { SubMenuItems } from "@/components/submenu"
 
 export default async function Work() {
 
@@ -23,9 +24,30 @@ export default async function Work() {
 		}
 	});
 
+	const dataCategories = await payload.find({
+		collection: 'worksCategory',
+	})
+	const categories = dataCategories.docs
+	const menuCategoriesItems = categories.map((cat) => {
+		return {
+			href: "/trabalho/categoria/" + cat.slug,
+			label: cat.title
+		}
+	})
+	menuCategoriesItems.unshift({ href: '/trabalho', label: "todos" })
 	return (
-
-		<WorkCard cards={rowCards} />
+		<>
+			<div className="relative flex justify-center w-full mt-8 mb-8 ">
+				<p className="z-10 w-full p-10 font-serif font-light text-center text-7xl text-neutral-dark-3 dark:text-neutral-light-1">
+					Criamos trabalhos bonitos, funcionais e com tecnologias avançadas que elevam e unificam as experiências em todas as superfícies da marca.
+				</p>
+			</div>
+			<div className={'flex flex-row items-center justify-start gap-4 px-10 py-10 bg-neutral-light-1'}>
+				<p>Filtros:</p>
+				<SubMenuItems items={menuCategoriesItems} />
+			</div>
+			<WorkCard cards={rowCards} />
+		</>
 
 	);
 }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
-
-
-const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['images.unsplash.com'],
+		remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
 	},
 	reactStrictMode: true,
 
